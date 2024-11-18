6cacaf88cc6db3d655de91fd51cdd6b0eefc5551
Dorjnyam Erdenetsogt <mjldoko11@gmail.com>
2024-11-18 21:40:07 +0800

    added biy daalt folder

--- script.js
+++ script.js
@@ -29,9 +29,69 @@ const labDetails = {
         <a href="https://lh5.googleusercontent.com/NvrtQExncBwxxMGG1pccsDRqqfxyATShO1M_ERQJtSoZtBG3vd9u6gkZ6JPqTV9z4O-sjr7J6KYhtHFSGBtEsVCrHbwMxzNMMeWHvpSCf_HKV9D_54qK0ChvDQ" target="_blank">Facebook Style Template</a>
         <p>Энэхүү загварыг гаргахад шаардлагатай бүхий л мэдлэгийг та бүхэн эзэмшсэн байгаа гэдэгт итгэлтэй байна. Бие даан судлаад жаахан жаахан CSS ашиглаж болно.</p>
     `,
-    lab4: "Lab 4: My CV - Build your personal CV using HTML and CSS.",
-    lab5: "Lab 5: Bootstrap with Responsive Design - Use Bootstrap to create a responsive website.",
-    lab6: "Lab 6: Flexbox Layout - Copy an existing website layout using Flexbox.",
+    lab4: `<h4>Лаборатори 4</h4>
+        <p>Энэ долоо хоногт та бүхнийг хоёр хэсэгтэй лабораторийн ажил гүйцэтгэж, лабораторийн оноогоо тооцуулахыг хүсч байна.</p>
+        <ol>
+            <li>
+                <p><a href="https://flukeout.github.io/" target="_blank">ЭНД</a> байгаа тоглоомыг тоглож давна. Энэ тоглоом нь <strong>CSS Selector</strong>-уудыг хэрэглэхэд суралцахад туслах ба энэ нь HTML элементүүдийг хялбархан барьж авах чадварыг хөгжүүлнэ.</p>
+                <p>Энэ даалгаврыг гүйцэтгэснээр 1 оноо авах боломжтой.</p>
+            </li>
+            <li>
+                <p>Дараа нь өөрийн <strong>CV дизайн</strong>-ыг гаргаж, 4 оноо авах боломжтой. Үүнд:</p>
+                <ul>
+                    <li><strong>Агуулга</strong>: 1 оноо</li>
+                    <li><strong>Өнгө, загвар</strong>: 1 оноо</li>
+                    <li>Үндсэн 2 оноо бүхий нийт 4 оноо олгогдоно.</li>
+                </ul>
+                <p>CV дизайн нь өөрийн өвөрмөц байдлыг харуулах бөгөөд интернетээс хуулж хийхгүй, татаж авахгүй байхыг анхаарна уу. Мөн <strong>Монгол өнгө төрх</strong> оруулж, өөрийн <strong>design taste</strong>-ийг тусгасан бол сайшаал хүртэх болно.</p>
+                <p>CV дизайны санаа авахад <a href="https://letmegooglethat.com/?q=cv+resume+design+top+10" target="_blank">ЭНД</a> хандаж болно.</p>
+            </li>
+        </ol>
+    `,
+    lab5: `<h4>Лаборатори 5</h4>
+        <p>Энэ долоо хоногийн лабораторийн ажил нь нийт 3 хэсгээс бүрдэх бөгөөд 5 оноо авах боломжтой.</p>
+
+        <ol>
+            <li>
+                <p>Зурагт үзүүлсэн вэб загварыг <strong>HTML</strong> ашиглан бүтцийн хувьд гаргах:</p>
+                <a href="http://webjunto.com/wp-content/uploads/2017/02/bootstrapexercise.png" target="_blank">Загвар зураг</a>
+                <p>Энэхүү хэсгийг гүйцэтгэснээр 2 оноо авна.</p>
+            </li>
+            <li>
+                <p>Дээрх зурагт <a href="http://webjunto.com/wp-content/uploads/2017/02/styleguide-exercise.png" target="_blank">стиль удирдамж</a>-д заагдсан өнгө болон фонтуудыг ашиглан вэб сайтаа загварын хувьд тохируулна уу.</p>
+                <p>Энэ хэсгийг гүйцэтгэснээр 2 оноо авна.</p>
+            </li>
+            <li>
+                <p><strong>@media query</strong> ашиглан вэб сайтаа гар утас, таблет, компьютер зэрэг дэлгэцийн хэмжээнүүдэд тааруулан responsive болгоно уу.</p>
+                <p>Энэхүү хэсгийг гүйцэтгэснээр 1 оноо авна.</p>
+            </li>
+        </ol>
+
+        <p>Нэмэлт мэдээлэл: Вэб сайтад дурын YouTube бичлэг оруулахдаа дараах алхмуудыг ашиглана:</p>
+        <ol>
+            <li>Оруулах бичлэг рүү очоод <strong>Share</strong> товчин дээр дарна.</li>
+            <li><strong>Embed</strong> товчийг сонгоод гарч ирсэн кодыг HTML дотроо хуулж оруулна.</li>
+        </ol>
+    `,
+    lab6: `
+        <h4>Лаборатори 6</h4>
+        <p>Энэ удаагийн лабораторийн ажил нь 2 хэсгээс бүрдэнэ:</p>
+        <ol>
+            <li>
+                <p><a href="https://flexboxfroggy.com/" target="_blank">Flexbox Froggy</a> тоглоомыг тоглон бүх үеийг нь амжилттай давж, flexbox-ийн талаар суралцах.</p>
+            </li>
+            <li>
+                <p>Flexbox-ийн мэдлэгээ ашиглан өөрийн хүссэн вэбсайтын нүүр хуудсыг дуурайлгаж хийх. Жишээ нь:</p>
+                <ul>
+                    <li><a href="https://eu.louisvuitton.com/eng-e1/homepage" target="_blank">Louis Vuitton</a></li>
+                    <li><a href="https://www.apple.com/" target="_blank">Apple</a></li>
+                    <li><a href="https://www.nba.com/" target="_blank">NBA</a></li>
+                </ul>
+                <p>Сонгосон вэбсайт нь хэт энгийн байхгүйг анхаарна уу.</p>
+            </li>
+        </ol>
+        <p>Энэхүү лабораторийн ажилд 2 долоо хоногийн хугацаа өгөх бөгөөд нийт оноог лабораторийн багш дүгнэнэ.</p>
+    `,
     lab7: `
             <h4>Лабораторийн ажил 7</h4>
             <p>Өмнөх лаб дээр хийсэн лаг гоё сайтынхаа нүүр хуудсыг онлайн байршуулаад вэб сайтын SEO шалгадаг tools-үүдээр шалгана.</p>
@@ -42,8 +102,59 @@ const labDetails = {
                 <li>80 болговол 4 оноо гэх мэт буураад явна.</li>
             </ul>
         `,
-    lab8: "Lab 8: JavaScript Basics - Learn the basics of JavaScript for interactivity.",
-    lab9: "Lab 9: Hangman with JavaScript - Create a Hangman game using JavaScript."
+    lab8: `
+        <h4>Лаборатори 8</h4>
+        <p>Энэ удаагийн лабораторийн ажил дараах 5 даалгавраас бүрдэнэ:</p>
+        <ol>
+            <li>
+                <p>Гараас өгсөн тооны цифрүүдийн нийлбэр нь палиндром мөн эсэхийг шалга.</p>
+            </li>
+            <li>
+                <p>Чоно 25 км/ц хурдтай, туулай 18 км/ц хурдтай. Гараас өгсөн зайтай байхад хэдэн минут, хэдэн секундын дараа гүйцэхийг ол.</p>
+            </li>
+            <li>
+                <p>Байшин 9 давхар, 3 орцтой, давхартаа 4 айлтай бол гараас өгсөн тоот нь хэд дүгээр орцны хэд дүгээр давхарын хэд дэх хаалга вэ?</p>
+            </li>
+            <li>
+                <p>Array-д өгөгдсөн 5 тооны хамгийн бага ерөнхий хуваагдагчийг ол.</p>
+            </li>
+            <li>
+                <p>Вэб сайт руу:</p>
+                <ul>
+                    <li>Өглөө ороход гараас өгсөн тооны 2 зэрэгийг ол.</li>
+                    <li>Орой ороход гараас өгсөн тооны язгуурыг ол.</li>
+                </ul>
+            </li>
+        </ol>
+    `,
+    lab9: `
+        <h4>Лаборатори 9</h4>
+        <p>Энэ удаагийн лабораторийн ажлаар <strong>hangman</strong> тоглоом хийх даалгавар өгөгдсөн.</p>
+        <p>Тоглоомын үндсэн шаардлагууд:</p>
+        <ol>
+            <li>
+                <p>Тоглоом 5 амьтай байна.</p>
+            </li>
+            <li>
+                <p>Array-д байрлах асуултуудаас <strong>random</strong>-оор нэг асуулт сонгогдоно.</p>
+            </li>
+            <li>
+                <p>Хэрэглэгч үсэг дарах бүрд:</p>
+                <ul>
+                    <li>Зөв бол <strong>баяр хүргэсэн</strong> мессеж харуулах.</li>
+                    <li>Буруу бол нэг эрхтэн нэмж зурагдаж тоглоом үргэлжилнэ.</li>
+                </ul>
+            </li>
+        </ol>
+        <p><strong>Онооны хуваарилалт:</strong></p>
+        <ul>
+            <li>Тоглоом зөв ажиллаж байвал - <strong>3 оноо</strong></li>
+            <li>Загвар дизайн сайтай бол - <strong>+1 оноо</strong></li>
+            <li>Responsive байвал - <strong>+1 оноо</strong></li>
+        </ul>
+        <p>Нийт: 5 оноо</p>
+        <p><strong>Амжилт!</strong></p>
+    `
 };
 
 function showSelectedLabDetails() {
